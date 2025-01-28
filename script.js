f8c58dc72ec5a54834482da2ee18305e1e751028
Louise210386 <loulouzoe2103@gmail.com>
2025-01-28 09:36:11 +0000

    wyp

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-function calculatrice(a, b, c) {
-    let operateur = c;
-    switch (operateur) {
-        case "+":
-            console.log(a+b);
-            break;
-        case "*":
-            console.log(a*b);
-            break;
-        case "-":
-            console.log(a-b);
-            break;
-        default:
-            console.log(a/b);
+// function calculatrice(a, b, c) {
+//     let operateur = c;
+//     switch (operateur) {
+//         case "+":
+//             console.log(a+b);
+//             break;
+//         case "*":
+//             console.log(a*b);
+//             break;
+//         case "-":
+//             console.log(a-b);
+//             break;
+//         default:
+//             console.log(a/b);
             
-        }
+//         }
       
-}
+
+
 
 // function calculatrice(a, b, c) {
 //     let result
@@ -34,4 +35,24 @@ function calculatrice(a, b, c) {
 //     }
 //     console.log(result)
 // }
+
+function calculatrice(){
+    let nb1=prompt("saisissez votre premier chiffre");
+    let nb2=prompt("saisissez votre deuxieme chiffre")
+    let operateur=prompt ("entrez le symbole de l'operation que vous souhaitez faire,+-*/")
+    switch(operateur){
+        case"+":
+        console.log(nb1+nb2);
+        break;
+        case"-":
+        console.log(nb1-nb2);
+        break;
+        case"x":
+        console.log(nb1*nb2)
+        break;
+        default:console.log(nb1/nb2)
+    }
+
+}
+
 console.log(calculatrice(40,10,"+"))
